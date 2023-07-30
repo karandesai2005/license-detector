928f234179f2e1eae474ce80c7f7aceecac93302
Karan Desai <89301880+karandesai2005@users.noreply.github.com>
2023-07-30 14:08:15 +0530

    Update main.py

--- automatic-number-plate/main.py
+++ automatic-number-plate/main.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 import numpy as np
 from sort.sort import Sort
-import util
+import csv
 
 results = {}
 
@@ -20,7 +20,7 @@ vehicles = [2, 3, 5, 7]
 # Read frames
 frame_nmr = -1
 ret = True
-while ret and frame_nmr < 5:  # Process only first 5 frames
+while ret and len(results) < 5:  # Process only until 5 unique frames
     frame_nmr += 1
     ret, frame = cap.read()
     if ret:
@@ -60,33 +60,4 @@ while ret and frame_nmr < 5:  # Process only first 5 frames
                 license_plate_text, license_plate_text_score = util.read_license_plate(license_plate_crop_thresh)
 
                 if license_plate_text is not None:
-                    results[frame_nmr][car_id] = {
-                        'frame_nmr': frame_nmr,
-                        'car_id': car_id,
-                        'car_bbox': [xcar1, ycar1, xcar2, ycar2],
-                        'license_plate_bbox': [x1, y1, x2, y2],
-                        'license_plate_bbox_score': score,
-                        'license_number': license_plate_text,
-                        'license_number_score': license_plate_text_score
-                    }
-                    print(f"License plate text: {license_plate_text}")
-
-# Write results to CSV
-output_file = './test.csv'
-column_names = [
-    'frame_nmr', 'car_id', 'car_bbox', 'license_plate_bbox', 'license_plate_bbox_score',
-    'license_number', 'license_number_score'
-]
-
-with open(output_file, 'w') as csv_file:
-    csv_file.write(','.join(column_names) + '\n')
-    for frame_nmr, frame_results in results.items():
-        for car_id, car_data in frame_results.items():
-            row_data = [str(frame_nmr)]
-            row_data.append(str(car_id))
-            row_data.append(','.join(map(str, car_data['car_bbox'])))
-            row_data.append(','.join(map(str, car_data['license_plate_bbox'])))
-            row_data.append(str(car_data['license_plate_bbox_score']))
-            row_data.append(car_data['license_number'])
-            row_data.append(str(car_data['license_number_score']))
-            csv_file.write(','.join(row_data) + '\n')
+                    results[frame_nmr][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
